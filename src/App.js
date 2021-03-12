74dd814a38e394cbe094c5f3ae4963cd510d8350
unknown <shin103502@naver.com>
2021-03-12 23:46:15 +0900

    hi

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React from 'react';
 
-function Food({name, picture}){
-  return <div>
-    
-   <h2> I like {name}</h2>
-  <img src={picture} />
-  </div>
-}
+
 
 const foodLike = [
-  {
+  { id: "1",
     name: "mando",
     image:"http://www.aeriskitchen.com/wp-content/uploads/2021/01/Flat_Dumplings_01-.jpg"
   },
-  {
+  { id: "2",
     name: "baechu",
     image:"http://www.aeriskitchen.com/wp-content/uploads/2021/01/Napa_Cabbage_Jeon_01-.jpg"
   },
-  {
+  { id: "3",
     name: "hoebbang",
     image:"http://www.aeriskitchen.com/wp-content/uploads/2021/01/Steamed_Vegetable_Bun_01-.jpg"
   }
 ];
 
 
+function Food({name, picture}){
+  return (
+    <div>  
+      <h2> I like {name}</h2>
+      <img src={picture} alt={name}/>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      {foodLike.map(dish => ( 
-      <Food name={dish.name} picture={dish.image}/>
+      {foodLike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
       </div>
+    
   );
 }
 
